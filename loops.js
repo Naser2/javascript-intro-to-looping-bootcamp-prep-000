52ad7742f8a47039af2518d5f22f6872d7e35026
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 23:06:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -36,32 +36,32 @@ function forLoop(array) {
 	return array;
 }
 
-// function whileLoop(n) {
-// 	while (n > 0) {
-// 		console.log(--n);
-// 	}
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
 
-// 	return 'done';
+	return 'done';
+}
+
+
+
+function doWhileLoop(array) {
+let i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+	}
+
+	do {
+		console.log('array.length = ' + array.length + ' and i = ' + i);
+		array = array.slice(1);
+		incrementVariable();
+	} while (array.length > 0 && i < 5);
+
+	return array;
+}
+
+// forEach(i in gifts) {
+//   console.log(gifts)
 // }
-
-
-
-// function doWhileLoop(array) {
-// let i = 0;
-
-// 	function incrementVariable() {
-// 		i = i + 1;
-// 	}
-
-// 	do {
-// 		console.log('array.length = ' + array.length + ' and i = ' + i);
-// 		array = array.slice(1);
-// 		incrementVariable();
-// 	} while (array.length > 0 && i < 5);
-
-// 	return array;
-// }
-
-// // forEach(i in gifts) {
-// //   console.log(gifts)
-// // }
